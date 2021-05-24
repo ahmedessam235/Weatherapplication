e2772acc3e6a64dd26188bbd8c651606f7842b49
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-05-24 17:33:21 +0200

    importing bootstrap and Inter font, finshing the header component with buttons

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header/Header";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./styles/Header.css";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Container";
+// import Col from "react-bootstrap/Container";
 function Header() {
   return (
-    <div className="Header">
-      <h1>INSTAWEATHER</h1>
+    <div className="Header ">
+            <h2>
+              <b>INSTAWEATHER</b>
+            </h2>
+            <div className="TemperatureButtons">    
+            <button id = "Celsius-button" class="btn btn-primary-outline" >C</button>
+            <button id = "Fahrenheit-button" class="btn btn-primary-outline">F</button>
+            </div>
     </div>
   );
 }
