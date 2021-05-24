342c92443e073d341b1ddb095ae5caf097f260b4
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-05-24 13:59:41 +0200

    fixing eslinter import erros and creating the first component

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BackgroundImage from "./BackgroundImage/BackgroundImage";
+function App() {
+  return (
+    <div>
+      <BackgroundImage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BackgroundImage/BackgroundImage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BackgroundImage() {
+  return <h3>test working now</h3>;
+}
+
+export default BackgroundImage;
