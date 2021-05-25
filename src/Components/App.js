1b2079a880c4563059f6db07ff34e151bcafdb91
ahmedessam235 <ahmedessameltantawy@gmail.com>
2021-05-25 13:50:22 +0200

    reading temperature data and saving it into a variable

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import Header from "./Header/Header";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// var temperatureReadings ;
 function App() {
-  /* this is a work around for the API because they have disabled the CORS this solutin will not work if the aplication is deployed */
-  const proxy = "https://cors-anywhere.herokuapp.com/";
-  const api = `${proxy}https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/30.0074,31.4913`; 
+  // function useTemperatureReadings(){
+  //   return temperatureReadings;
+  // }
+  function saveTemperatureData (temperatureData){
+    console.log(temperatureData," test test test");
+    // temperatureReadings = temperatureData;
+  }
+  function getTemperatureData(){
 
-  fetch(api)
-    .then((response) => {
+    /* this is a work around for the API because they have disabled the CORS this solutin will not work if the aplication is deployed */
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const api = `${proxy}https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/30.0074,31.4913`; 
+    
+    fetch(api)
+      .then((response) => {
       return response.json();
-    })
-    .then((data) => {
-      console.log("data is streaming");
-      console.log(data);
-    });
-
+   
+      })
+      .then((temperatureData) => {
+        saveTemperatureData(temperatureData);
+        
+      });
+      
+   }
+   getTemperatureData();
 
   return (
     <div>
